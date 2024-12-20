8eb5784ef16f54a1d1f7e9d02b0dd6a60019d233
giman2 <gimanbag2@gmail.com>
2024-12-20 19:48:27 +0900

    final

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,42 +2,19 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("com.google.gms.google-services")
-    kotlin("kapt") // 이 줄을 추가하세요.
 }
 
 dependencies {
-    implementation ("androidx.recyclerview:recyclerview:1.1.0")
-    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
-    implementation("com.google.firebase:firebase-analytics")
-    implementation ("com.google.firebase:firebase-storage")
-    implementation ("com.google.firebase:firebase-firestore:24.7.0")
-    implementation ("com.google.firebase:firebase-firestore:24.7.0")
-    implementation("com.github.bumptech.glide:glide:4.15.1")
-    implementation(libs.androidx.material3.android)
-    implementation(libs.firebase.database.ktx)
-    implementation(libs.androidx.databinding.runtime)
-    kapt("com.github.bumptech.glide:compiler:4.15.1")
-
-    implementation(libs.androidx.ui.android)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.firebase.auth.ktx)
-    implementation(libs.firebase.firestore.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
 }
 
-
 android {
-    namespace = "com.Refee.RefeeDB"
+    namespace = "com.example.firebaselogin"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.Refee.RefeeDB"
+        applicationId = "com.example.firebaselogin"
         minSdk = 29
         targetSdk = 35
         versionCode = 1
@@ -62,4 +39,17 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
